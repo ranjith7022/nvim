f1c69eca8edefdb7907fa95965632bc258ba9b72
Ranjith <mailranjith7022@gmail.com>
2025-06-27 21:25:55 +0530

    python lsp for venv , but need to fixed more

---
+++ lua/custom/plugins/python.lua
@@ -0,0 +1,20 @@
+
+return {
+  "neovim/nvim-lspconfig",
+  opts = {
+    servers = {
+      pyright = {
+        settings = {
+          python = {
+            pythonPath = "/usr/bin/python3",
+            analysis = {
+              autoSearchPaths = true,
+              useLibraryCodeForTypes = true,
+              diagnosticMode = "workspace",
+            },
+          },
+        },
+      },
+    },
+  },
+}
